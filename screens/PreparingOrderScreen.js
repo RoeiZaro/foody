a320cd6c2912be44d3dd1708146c241e4931f043
roei zaro <roeip21@gmail.com>
2023-07-12 17:36:18 +0300

    build comp

---
+++ screens/PreparingOrderScreen.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Animatable from "react-native-animatable";
+
+const PreparingOrderScreen = () => {
+  return (
+    <SafeAreaView className="bg-[#00CCBB] flex-1 justify-center items-center">
+      <Animatable.Image
+        source={require("../elements/loadingStir.gif")}
+        animation="slideInUp"
+        iterationCount={1}
+        className="h-96 w-96"
+      />
+      <Text>PreparingOrderScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default PreparingOrderScreen;
