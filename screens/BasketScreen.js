7a1e20ea71e8f2e409be5306bc3e90546864b78f
roei zaro <roeip21@gmail.com>
2023-07-12 10:48:55 +0300

    styling!

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,11 +1,40 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
+import React, { useMemo, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useDispatch, useSelector } from "react-redux";
+import { selectRestaurant } from "../reduxSlices/restaurantSlice";
+import { selectBasketItems } from "../reduxSlices/basketSlice";
 
 const BasketScreen = () => {
+  const navigation = useNavigation();
+  const restaurant = useSelector(selectRestaurant);
+  const items = useSelector(selectBasketItems);
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState([]);
+  const dispatch = useDispatch();
+
+  useMemo(() => {
+    const groupedItems = items.reduce((results, item) => {
+      (results[item.id] = results[item.id] || []).push(item);
+      return results;
+    }, {});
+
+    setGroupedItemsInBasket(groupedItems);
+  }, [items]);
+
   return (
-    <View>
-      <Text>BasketScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <View>
+          <View>
+            <Text className="text-lg font-bold text-center">Basket</Text>
+            <Text className="text-center text-gray-400">
+              {restaurant.title}
+            </Text>
+          </View>
+          <TouchableOpacity>onp</TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
