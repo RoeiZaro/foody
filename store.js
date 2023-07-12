96969415aba0d417785099b2a8fc83a678bbb37f
roei zaro <roeip21@gmail.com>
2023-07-12 10:47:22 +0300

    adding restuarant reducer

--- store.js
+++ store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "./reduxSlices/basketSlice";
-
+import restaurantReducer from "./reduxSlices/restaurantSlice";
 export const store = configureStore({
   reducer: {
     // items basket reducer
     basket: basketReducer,
+    restaurant: restaurantReducer,
   },
 });
