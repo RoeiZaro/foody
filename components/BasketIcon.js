12ac66f744fdd473113c390308d4253080d76cac
roei zaro <roeip21@gmail.com>
2023-07-12 09:14:25 +0300

    finishing pop up

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
 import {
@@ -6,6 +6,7 @@ import {
   selectBasketTotalPrice,
 } from "../reduxSlices/basketSlice";
 import { useNavigation } from "@react-navigation/native";
+import Currency from "react-currency-formatter";
 
 const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
@@ -13,8 +14,18 @@ const BasketIcon = () => {
   const basketTotalPrice = useSelector(selectBasketTotalPrice);
 
   return (
-    <View>
-      <Text>BasketIcon</Text>
+    <View className="absolute bottom-2 w-full z-50">
+      <TouchableOpacity className="flex-row mx-5 bg-[#00CCBB] p-4 rounded-lg items-center space-x-1">
+        <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2 rounded-md">
+          {items.length}
+        </Text>
+        <Text className="flex-1 text-white font-extrabold text-lg text-center">
+          View Basket
+        </Text>
+        <Text className="text-lg text-white font-extrabold">
+          <Currency quantity={basketTotalPrice} currency="ILS" />
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
