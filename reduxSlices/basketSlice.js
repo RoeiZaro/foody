8b909770f44c8cfa25611bd5a9264763328329bf
roei zaro <roeip21@gmail.com>
2023-07-12 08:47:15 +0300

    adding total price function

--- reduxSlices/basketSlice.js
+++ reduxSlices/basketSlice.js
@@ -33,7 +33,11 @@ export const basketSlice = createSlice({
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 export const selectBasketItems = (state) => state.basket.items;
+
 export const selectBasketItemsById = (state, id) =>
   state.basket.items.filter((item) => item.id === id);
+
+export const selectBasketTotalPrice = (state) =>
+  state.basket.item.reduce((total, item) => (total += item.price), 0);
 
 export default basketSlice.reducer;
