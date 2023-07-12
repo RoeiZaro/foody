29cd1c7289e7c09938c79bb0d4c88f1e239b3bed
roei zaro <roeip21@gmail.com>
2023-07-12 08:47:25 +0300

    minor updates

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -2,9 +2,11 @@ import { Text, View } from "react-native";
 import React, { Component } from "react";
 import { useSelector } from "react-redux";
 import { selectBasketItems } from "../reduxSlices/basketSlice";
+import { useNavigation } from "@react-navigation/native";
 
 const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
+  const navigation = useNavigation();
 
   return (
     <View>
