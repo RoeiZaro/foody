d310a0ffd2fe4472da05282c27c8b87c85b655ef
roei zaro <roeip21@gmail.com>
2023-07-12 08:56:40 +0300

    fixing reducer problem

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -1,12 +1,16 @@
 import { Text, View } from "react-native";
-import React, { Component } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import { selectBasketItems } from "../reduxSlices/basketSlice";
+import {
+  selectBasketItems,
+  selectBasketTotalPrice,
+} from "../reduxSlices/basketSlice";
 import { useNavigation } from "@react-navigation/native";
 
 const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
   const navigation = useNavigation();
+  const basketTotalPrice = useSelector(selectBasketTotalPrice);
 
   return (
     <View>
